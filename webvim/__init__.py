38e68847f824189d72291d322a9ebaaca86d3e37
Britt Gresham <brittcgresham@gmail.com>
2014-05-10 17:44:20 -0500

    Added terminal resizing

--- webvim/__init__.py
+++ webvim/__init__.py
@@ -1,12 +1,17 @@
 from pyramid.config import Configurator
 
+from webvim.assets.websocket import TerminalClient
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    config.include('pyramid_sockjs')
     config.include('pyramid_chameleon')
+
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_sockjs_route(prefix='/terminal', session=TerminalClient)
     config.scan()
     return config.make_wsgi_app()
---
+++ webvim/assets/websocket.py
@@ -0,0 +1,41 @@
+import time
+
+from pyramid_sockjs.session import Session
+
+import termio
+
+command = "docker run -i -t vim_base %s"
+command %= "timelimit -pq -T 600 -S 9 vim -Z ./README.md"
+
+
+class TerminalClient(Session):
+
+    def on_open(self):
+        self.ready = False
+        self.mp = termio.Multiplex(command)
+        self.mp.spawn()
+        while not self.mp.isalive():
+            time.sleep(.1)
+        self.mp.resize(cols=120, rows=40)
+        self.send_new_data()
+        pass
+
+    def on_message(self, message):
+        if message[0] == "0":
+            self.mp.write(unicode(message[1:]))
+        if message[0] == "1":
+            rows = message[1:].split(",")[0]
+            cols = message[1:].split(",")[1]
+            self.mp.resize(int(cols), int(rows))
+        self.send_new_data()
+
+    def on_close(self):
+        if self.mp.isalive():
+            self.mp.write(u"\u001B\u001B:q!\u000A")
+            self.mp.terminate()
+
+    def send_new_data(self):
+        if self.mp.isalive():
+            self.send(self.mp.read())
+        else:
+            self.close()
