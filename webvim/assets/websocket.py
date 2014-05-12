e0166f63cd70accf2d13da8ef3a3db16bbd51e45
Britt Gresham <brittcgresham@gmail.com>
2014-05-11 20:26:14 -0500

    Fixed timelimit and added a thread that monitors for activity

--- webvim/assets/websocket.py
+++ webvim/assets/websocket.py
@@ -1,41 +1,60 @@
 import time
+import threading
 
 from pyramid_sockjs.session import Session
 
 import termio
 
 command = "docker run -i -t demophoon/vim_base %s"
-command %= "timelimit -pq -T 600 -S 9 vim -Z ./README.md"
+command %= "timelimit -q -t 1800 -S 9 vim -Z ./README.md && exit"
 
 
 class TerminalClient(Session):
 
     def on_open(self):
-        self.ready = False
+        self.last_update = time.time()
         self.mp = termio.Multiplex(command)
         self.mp.spawn()
+        self.timer = threading.Thread(target=self.send_new_data)
         while not self.mp.isalive():
             time.sleep(.1)
         self.mp.resize(cols=120, rows=40)
-        self.send_new_data()
+        self.timer.start()
         pass
 
     def on_message(self, message):
+        self.last_update = time.time()
+        if not self.mp.isalive():
+            self.close()
         if message[0] == "0":
             self.mp.write(unicode(message[1:]))
         if message[0] == "1":
             rows = message[1:].split(",")[0]
             cols = message[1:].split(",")[1]
             self.mp.resize(int(cols), int(rows))
-        self.send_new_data()
 
     def on_close(self):
         if self.mp.isalive():
-            self.mp.write(u"\u001B\u001B:q!\u000A")
-            self.mp.terminate()
+            self.mp.writeline(u"\u001B\u001B:q!")
+        self.mp.terminate()
 
     def send_new_data(self):
-        if self.mp.isalive():
-            self.send(self.mp.read())
-        else:
-            self.close()
+        while self.mp.isalive():
+            output = None
+            try:
+                output = self.mp.read()
+            except Exception, e:
+                pass
+            current_time = time.time()
+            if output:
+                self.send(output)
+                self.last_update = current_time
+            idle_time = current_time - self.last_update
+            if idle_time > 600:
+                break
+            elif idle_time > 15:
+                time.sleep(1)
+            else:
+                time.sleep(.1)
+        self.mp.terminate()
+        self.close()
